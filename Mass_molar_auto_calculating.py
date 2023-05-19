3355e4ef2d737865486da1fe115df22aac61f509
nauramaulidya12 <130137473+nauramaulidya12@users.noreply.github.com>
2023-05-19 21:36:10 +0700

    Add files via upload

--- Mass_molar_auto_calculating.py
+++ Mass_molar_auto_calculating.py
@@ -15,15 +15,15 @@ with col5:
     st.markdown('''<div class='Anggota Classic7'>Nida Nafilah 2219134</div>''', unsafe_allow_html=True)
 
 with st.sidebar:
-    selected = option_menu("Aplikasi Massa Molekul dan Stoikiometri Sederhana", ['Massa Molekul', 'Perhitungan Molaritas','Perhitungan Normalitas','Perhitungan Mol',], 
+    selected = option_menu("Aplikasi Berat Molekul dan Stoikiometri Sederhana", ['Berat Molekul', 'Perhitungan Molaritas','Perhitungan Normalitas','Perhitungan Mol',], 
           default_index=1)
-if (selected == "Massa Molekul"):
-    st.title("Aplikasi Perhitungan Massa Molekul dan Stoikiometri Sederhana")
-    st.write("*Untuk meminimalisir adanya kesalahan saat penggunaan, kami mengubah simbol beberapa element penggunaan")
+if (selected == "Berat Molekul"):
+    st.title("Aplikasi Perhitungan Berat Molekul dan Stoikiometri Sederhana")
+    st.write("*Agar tidak bosan kami sedikit mengubah simbol beberapa element penggunaan nya, dimaksudkan agar murid murid juga senang menghitung Massa Molekul Relatif dengan manual")
     st.write("Berikut adalah Keyword untuk beberapa element", "He=q" ,"Li=w" ,"Be=r" ,"Fe=t" ,"Na=y" ,"Mg=u" ,"Ca=i" ,"Ne=o" ,"Al=p" ,"Si=a" ,"Cl=s" ,"Ar=d" ,"Ti=f" ,"Cr=g" ,"Mn=h" ,"Co=j" ,"Ni=k" ,"Cu=l" ,"Zn=z" ,"As=x" ,"Br=b" ,"Pd=c" ,"Ag=v" ,"Cd=n" ,"Ba=m" ,"Pt=[","Au=]","Hg=@","Pb=^")
     st.write("Untuk atom lain yang tidak tercantum ditulis seperti biasa contoh pada NaOH Na ditulis sebagai y sedangkan OH tetap jadi untuk penulisan jadi yOH=NaOH")
     rumus = st.text_input("Masukkan rumus molekul senyawa: ")
-    massa_atom = {"H": 1.008, "q": 4.003, "w": 6.941, "r": 9.012, "B": 10.81, "C": 12.01, "N": 14.01, "O": 16.00,"F": 19,"t": 56.03,"y": 23.001,"K": 39.098,"u": 24.305,"i": 40.078,"S": 32.06,"o": 20.18, "p": 26.98, "a": 28.08, "P": 30.97, "s": 35.5, "d": 40.00, "f": 48.0, "V": 50.94, "g": 52.00, "h": 54.94,"j": 58.93, "k": 58.70, "l": 63.55, "z":65.40, "x": 74.92, "b": 79.90, "c": 106.42, "v": 107.86, "n": 112.41, "Sn": 118.71, "I": 126.90, "m": 137.33, "[":195.08,"]":196.97,"@":200.59,"^":207.2}
+    massa_atom = {"H": 1.008, "He": 4.003, "Li": 6.941, "Be": 9.012, "B": 10.81, "C": 12.01, "N": 14.01, "O": 16.00,"F": 19,"Fe": 56.03,"Na": 23.001,"K": 39.098,"Mg": 24.305,"Ca": 40.078,"S": 32.06,"o": 20.18, "p": 26.98, "a": 28.08, "P": 30.97, "s": 35.5, "d": 40.00, "f": 48.0, "V": 50.94, "g": 52.00, "h": 54.94,"j": 58.93, "k": 58.70, "l": 63.55, "z":65.40, "x": 74.92, "b": 79.90, "c": 106.42, "v": 107.86, "n": 112.41, "Sn": 118.71, "I": 126.90, "m": 137.33, "[":195.08,"]":196.97,"@":200.59,"^":207.2}
     
     total_massa = 0
     # Loop melalui rumus untuk menghitung jumlah atom setiap unsur
@@ -61,26 +61,22 @@ if (selected == "Massa Molekul"):
 if (selected == "Perhitungan Molaritas"):
     st.title("Perhitungan Molaritas")
     
-    st.write("Molaritas merupakan salah satu ukuran kelarutan yang menyatakan jumlah mol suatu zat per volume larutan. Molaritas ini disimbolkan dengan huruf M dengan satuannya milar atau M yang setara dengan mol/liter.")
-    
     Berat = st.number_input("Masukkan Berat (g):")
     Mr = st.number_input("Masukkan Mr:")
     Volume = st.number_input("Masukkan Volume (L)")
     button = st.button ("Hitung Konsentrasi Dalam Molaritas (mol/L")
-    konsentrasi_sampel = (Berat/(Mr*Volume))
+    konsentrasi_sampel = ((Berat/Mr)*Volume)
     if button:
         st.write(f"Konsentrasi Sampel adalah",konsentrasi_sampel,"mol/L")
 
 if (selected == "Perhitungan Normalitas"):
     st.title("Perhitungan Normalitas")
     
-    st.write("Normalitas adalah ukuran yang menunjukkan konsentrasi pada berat setara dalam gram per liter larutan. Berat ekivalen itu sendiri adalah ukuran kapasitas reaktif molekul yang dilarutkan dalam larutan.")
-    
     Berat = st.number_input("Masukkan Berat (g):")
     BE = st.number_input("Masukkan BE:")
     Volume = st.number_input("Masukkan Volume (L)")
     button = st.button ("Hitung Konsentrasi Dalam Normalitas (grek/L)")
-    konsentrasi_sampel = (Berat/(BE*Volume))
+    konsentrasi_sampel = ((Berat/BE)*Volume)
     if button:
         st.write(f"Konsentrasi Sampel adalah",konsentrasi_sampel,"grek/L")
 
